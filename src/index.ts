7d1edee0443e052d2277850fc68faa3476624d1b
XanderUZZZZER <xanderuzzzer@gmail.com>
2020-12-09 23:21:16 +0200

    basic store setup

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const initialState = {
 
 const counterReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'counter/incremeted':
+    case 'counter/incremented':
       return { ...state, value: state.value + 1 }
     case 'counter/decremented':
       return { ...state, value: state.value - 1 }
@@ -17,7 +17,37 @@ const counterReducer = (state = initialState, action) => {
 
 const store = createStore(counterReducer)
 
+
+// Handle user inputs by "dispatching" action objects,
+// which should describe "what happened" in the app
+document.getElementById('increment').addEventListener('click', function () {
+  store.dispatch({ type: 'counter/incremented' })
+});
+
+document.getElementById('decrement').addEventListener('click', function () {
+  store.dispatch({ type: 'counter/decremented' })
+});
+
+document
+  .getElementById('incrementIfOdd')
+  .addEventListener('click', function () {
+    // We can write logic to decide what to do based on the state
+    if (store.getState().value % 2 !== 0) {
+      store.dispatch({ type: 'counter/incremented' })
+    }
+  });
+
+document
+  .getElementById('incrementAsync')
+  .addEventListener('click', function () {
+    // We can also write async logic that interacts with the store
+    setTimeout(function () {
+      store.dispatch({ type: 'counter/incremented' })
+    }, 1000)
+  });
+
 const valueEl = document.getElementById('value')
+
 
 const render = () => {
   const state = store.getState()
@@ -27,5 +57,6 @@ const render = () => {
 render()
 
 store.subscribe(render)
+store.subscribe(() => console.log('action dispatched', store.getState().value))
 
 console.log('index end');
